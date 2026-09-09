ef8a4bf6e735a8fc3a1ff53f0fe6e272a221accf
yuvaraj337 <rayalarhonda777@gmail.com>
2026-09-09 22:46:43 +0530

    Initial commit

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -1,7 +1,7 @@
-import heroFarm from '../../../reference/homepage/hero-farm.jpeg';
-import heroVilla from '../../../reference/homepage/hero-villa.jpeg';
-import heroPlots from '../../../reference/homepage/hero-apartments.jpeg';
-import heroApartments from '../../../reference/homepage/hero-plots.jpeg';
+import heroFarm from '../../assets/hero/hero-farm.jpeg';
+import heroVilla from '../../assets/hero/hero-villa.jpeg';
+import heroPlots from '../../assets/hero/hero-apartments.jpeg';
+import heroApartments from '../../assets/hero/hero-plots.jpeg';
 
 export const heroSlidesData = [
   {
